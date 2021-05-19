811c473b8f9125c45e8316ed034d0f5c35f1c447
RKPasha <rehankhalid5544@gmail.com>
2021-05-19 17:09:26 +0500

    backend1

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,4 +1,33 @@
-
 function test() {
 alert("Anni deya yad rakhna si naa Password🙄!!!");
+}
+
+function goToCeoPage() {
+    alert("CEO credentials entered");
+    //location.href='/CEO/Index';
+    window.open('/CEO/Index'); return true;
+}
+
+function goToManagerPage() {
+    location.href('/Manager/Index');
+    alert("Manager credentials entered");
+}
+
+function goToUserPage() {
+    //location.href('/CEO/Index');
+    alert("User Login");
+}
+function Login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (email === "ceo@duko.com" && password === "ceo") {
+        goToCeoPage();
+    } else if (email === "manager@duko.com" && password === "manager") {
+        goToManagerPage();
+    } else if (email === "user@duko.com" && password === "user") {
+        goToUserPage();
+    } else {
+        alert("User Not Found!!");
+    }
 }
