5d15815724f3b7e5d18ffec81eb7f98db8465f3f
RKPasha <rehankhalid5544@gmail.com>
2021-05-26 14:08:16 +0500

    web api integrated

---
+++ Models/Managers_Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Managers_Data : Person
+    {
+        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override DateTime DateOfBirth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string BranchName { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+	public abstract class Person
+	{
+        public abstract string Name { get; set; }
+        public abstract DateTime DateOfBirth { get; set; }
+        public abstract string Email { get; set; }
+        public abstract string Password { get; set; }
+    }
+}
