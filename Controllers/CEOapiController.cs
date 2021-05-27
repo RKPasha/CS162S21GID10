10ec0c2bb21e9e9f420aca69e52db3c7c216cf0a
RKPasha <rehankhalid5544@gmail.com>
2021-05-27 22:32:24 +0500

    checkDatabase

---
+++ Controllers/CEOapiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CEOapiController : ApiController
+    {
+        private static CEOapiController obj;
+
+        private List<Managers_Data> list;
+
+        private CEOapiController()
+        {
+            list = new List<Managers_Data>();
+        }
+
+        public static CEOapiController getObject()
+        {
+            if (obj == null)
+            {
+                obj = new CEOapiController();
+            }
+            return obj;
+        }
+
+        [HttpPost]
+        public void addEmployee(Managers_Data M)
+        {
+            list.Add(M);
+        }
+
+        [HttpPost]
+        public List<Managers_Data> getAllManagers()
+        {
+            return list;
+        }
+    }
+}
