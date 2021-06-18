cec92127745e6987a985b2eae61aacd61faee5c8
Muneeb912-cmd <81864431+Muneeb912-cmd@users.noreply.github.com>
2021-06-18 03:48:44 -0700

    UserData Display

--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class ManagerController : Controller
     {
+        DukoWheelsDBEntities1 db = new DukoWheelsDBEntities1();
+        List<UserData> list = new List<UserData>();
         // GET: Manager
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult ViewAllUsers()
+        {
+            var users = db.UserDBTables.ToList();
+            foreach (var dbObj in users)
+            {
+                UserData md = new UserData();
+                md.Name = dbObj.Name;
+                md.DateOfBirth = dbObj.DateOfBirth;
+                md.Email = dbObj.Email;
+                md.Password = dbObj.Password;
+                md.ContactNumber = dbObj.Contact;
+                list.Add(md);
+            }
+            return View(list);
         }
     }
 }
--- Models/UserData.cs
+++ Models/UserData.cs
@@ -5,13 +5,9 @@ using System.Web;
 
 namespace WebApplication1.Models
 {
-    public class UserData 
+    public class UserData : Person
     {
-        public string Name { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ContactNumber { get; set; }
+        
         
     }
 }
