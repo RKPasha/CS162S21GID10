46bcc9dbcf809e5e04d35915c99488f48bbc0ef3
RKPasha <rehankhalid5544@gmail.com>
2021-06-20 01:03:30 +0500

    Manager Crud operations + Contact Form and card view

--- Models/Managers_Data.cs
+++ Models/Managers_Data.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,10 @@ namespace WebApplication1.Models
 {
     public class Managers_Data : Person
     {
+        [Required(ErrorMessage = "Branch Name can't be empty..")]
         public string BranchName { get; set; }
+
+        [Required(ErrorMessage = "Cnic No Field must be filled..")]
         public string CnicNumber { get; set; }
     }
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,23 @@ namespace WebApplication1.Models
 {
 	public class Person
 	{
+        [Required(ErrorMessage = "Name Field must be filled..")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Select Date of Birth..")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Email Field must be filled..")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password Field must be filled..")]
+        [MinLength(8, ErrorMessage = "Password must be 8 characters long..")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Contact Field must be filled..")]
+        [MinLength(11, ErrorMessage = "Contact No must be 11 digits long..")]
         public string ContactNumber { get; set; }
     }
 }
